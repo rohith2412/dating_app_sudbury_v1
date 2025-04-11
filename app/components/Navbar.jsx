3ce9720d60a13aff03e293df98b70f5f28757141
rohith <rohithra75@gmail.com>
2025-04-11 13:31:21 -0400

    added few more assets

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,41 +1,43 @@
 export function Navbar() {
   return (
-    <div>
+    <div className="m-3">
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
       </style>
 
-      <div className="flex">
-        <div className="pl-4 md:pl-10 pt-4 md:pt-6 flex items-center gap-4">
+      <div className="flex ">
+        <div className="pl-15  pt-4  flex items-center gap-4">
           <img
             src="/st1.png"
             alt="logo"
-            className="transform scale-x-[-1] w-10 h-10 md:w-10 md:h-10 "
+            className="transform scale-x-[-1] w-10 h-10  "
           />
-          <h2 className="lilita-one-regular text-xl md:text-3xl">ASTRA</h2>
+          <h2 className="lilita-one-regular text-xl ">ASTRA</h2>
         </div>
-        <div className="flex pl-50">
-        <div className="border flex justify-center rounded-2xl pr-4 pl-4 ">
-          <button>Sigin In</button>
-        </div>
+        <div className="pr-15 flex w-full justify-end  h-12  ">
+          <div className="join border flex  justify-center rounded-4xl  pr-4 pl-4 ">
+            <button className="lilita-one-regular font-bold text-black tracking-tight">
+              Join Now
+            </button>
+          </div>
         </div>
         {/* <div className="">
-          <button className="AstraPro ">
-            <div className="wrapper">
-              <span>Get AstraPro</span>
-              <div className="circle circle-12"></div>
-              <div className="circle circle-11"></div>
-              <div className="circle circle-10"></div>
-              <div className="circle circle-9"></div>
-              <div className="circle circle-8"></div>
-              <div className="circle circle-7"></div>
-              <div className="circle circle-6"></div>
-              <div className="circle circle-5"></div>
-              <div className="circle circle-4"></div>
-              <div className="circle circle-3"></div>
-              <div className="circle circle-2"></div>
-              <div className="circle circle-1"></div>
+          <button class="AstraPro">
+            <div class="wrapper">
+              <span>UIVERSE</span>
+              <div class="circle circle-12"></div>
+              <div class="circle circle-11"></div>
+              <div class="circle circle-10"></div>
+              <div class="circle circle-9"></div>
+              <div class="circle circle-8"></div>
+              <div class="circle circle-7"></div>
+              <div class="circle circle-6"></div>
+              <div class="circle circle-5"></div>
+              <div class="circle circle-4"></div>
+              <div class="circle circle-3"></div>
+              <div class="circle circle-2"></div>
+              <div class="circle circle-1"></div>
             </div>
           </button>
         </div> */}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Minibar from "./components/Minibar";
 import { Navbar } from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Minibar />
       <Navbar />
     </div>
   );
