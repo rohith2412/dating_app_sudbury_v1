5e714bfe62debd34a68940554982306ff5a78cb2
rohith <rohithra75@gmail.com>
2025-05-10 09:15:41 -0400

    added dockerfile

--- app/components/Heading.jsx
+++ app/components/Heading.jsx
@@ -4,10 +4,8 @@ export default function Heading() {
   return (
     <div>
       <h1 className="pt-20 lg:p-15 text-gray-300 lg:mt-6 w-90  text-6xl text-start lg:w-fit  md:text-6xl lg:text-8xl font-semibold mx-auto   lg:text-center z-10  tracking-tighter pb-0">
-        Join Our Malayali<br />Dating 💌 Community <br /> In <span className=" text-red-400 underline ">Sudbury</span>📍
+        Join Our Malayali<br />Dating💌Community<br />In<span className="text-red-400 underline ">Sudbury</span>📍
       </h1>
     </div>
-
   );
 }
-d
