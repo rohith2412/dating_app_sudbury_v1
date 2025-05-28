5d2baf8e9593f3e74b21de1b5e0c24208533dd12
rohith <rohithra75@gmail.com>
2025-05-28 01:08:10 -0400

    added seo

--- app/page.js
+++ app/page.js
@@ -1,15 +1,31 @@
-
 import Analytics from "./components/Analytics";
 import CookieConsentBanner from "./components/CookieConsentBanner";
 import Landing from "./landing/page";
+
+export const metadata = {
+  title: "Malayali Dating App in Sudbury | Astra",
+  description:
+    "Join Astra - the exclusive Malayali dating community in Sudbury. Connect, chat, and find your perfect partner today!",
+  keywords: [
+    "Malayali dating",
+    "Sudbury dating",
+    "Astra app",
+    "Malayali singles",
+    "Malayali community Sudbury",
+  ],
+  alternates: {
+    canonical: "https://dating-app-sudbury-v1.vercel.app/",
+  },
+};
+
 
 export default function Home() {
   return (
     <div>
       
-      <Landing />
-      <CookieConsentBanner />
-      <Analytics />
+        <Landing />
+        <CookieConsentBanner />
+        <Analytics />
 
     </div>
   );
